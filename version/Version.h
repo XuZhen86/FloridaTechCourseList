0ba6cf9134dc9da0fad814d9528e6ee0722eec45
Zhen Xu <curt_xu@me.com>
2019-07-23 21:57:58 +0800

    Add git commit sha1 tracking in versioning script

--- version/Version.h
+++ version/Version.h
@@ -3,11 +3,13 @@
 
 #include<QString>
 
-static const QString versionString="0.0.1.12";
+static const QString versionString="0.0.2.21";
 
 static const QString versionMajor="0";
 static const QString versionMinor="0";
-static const QString versionPatch="1";
-static const QString versionBuild="12";
+static const QString versionPatch="2";
+static const QString versionBuild="21";
+
+static const QString lastCommitSha1="68fc6a619955337c8dfd0d752a51d5223d55f7a6";
 
 #endif
--- version/Version.py
+++ version/Version.py
@@ -1,3 +1,5 @@
+import subprocess
+
 file=open('version/Version.txt','r')
 major,minor,patch,build=[int(val) for val in file.readlines()]
 file.close()
@@ -8,6 +10,8 @@ print('Version {:d}.{:d}.{:d}.{:d}'.format(major,minor,patch,build))
 file=open('version/Version.txt','w')
 file.write('{:d}\n{:d}\n{:d}\n{:d}\n'.format(major,minor,patch,build))
 file.close()
+
+gitSha1=subprocess.run(['git','rev-parse','HEAD'],stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1]
 
 file=open('version/Version.h','w')
 file.write('#ifndef VERSION_H\n')
@@ -21,6 +25,8 @@ file.write('static const QString versionMajor="{:d}";\n'.format(major))
 file.write('static const QString versionMinor="{:d}";\n'.format(minor))
 file.write('static const QString versionPatch="{:d}";\n'.format(patch))
 file.write('static const QString versionBuild="{:d}";\n'.format(build))
+file.write('\n')
+file.write('static const QString lastCommitSha1="{}";\n'.format(gitSha1))
 file.write('\n')
 file.write('#endif')
 file.write('\n')
